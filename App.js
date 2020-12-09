2398d9d654a51efdccd86459156d02cbe0e0b426
KanoKit <kano.kitaguchi@gmail.com>
2020-12-08 20:00:37 -0500

    .

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 import SummaryScreen from './screens/SummaryScreen';
